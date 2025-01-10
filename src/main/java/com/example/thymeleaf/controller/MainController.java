ffab0d8add63ae481ddc0f93258ef2b928dabc78
Nuray <nuraymuxtarli67@gmail.com>
2025-01-10 13:17:49 +0400

    Initial commit

---
+++ src/main/java/com/example/thymeleaf/controller/MainController.java
@@ -0,0 +1,22 @@
+package com.example.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+
+    @GetMapping("/home")
+    public String Hello(Model model){
+        model.addAttribute("name","Thymeleaf");
+        return "index";
+    }
+
+    @GetMapping("/elvis")
+    public String elvis(Model model){
+        model.addAttribute("isAdmin",true);
+        return "elvis";
+    }
+}
